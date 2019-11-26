a715ee391d19efc4c66b4445f3cf01bb8f0c4d57
James A. Black Jr <mrdesigner@jamesblackjr.com>
2019-11-26 17:23:50 -0600

    Add Support for GraphQL Context in GetUser Decorator.

--- src/schematics/auth-module/files/ts/get-user.decorator.ts
+++ src/schematics/auth-module/files/ts/get-user.decorator.ts
@@ -1,6 +1,10 @@
 import { createParamDecorator } from '@nestjs/common';
 import { User } from './user.entity';
 
-export const GetUser = createParamDecorator((data, req): User => {
-  return req.user;
+export const GetUser = createParamDecorator((data, params): User => {
+  if (Array.isArray(params) && params.length) {
+    return params[2].req.user;
+  } else {
+    return params.user;
+  }
 });
